405166e6dc5bced82eb75db56557166c0a2e2fb1
heewon0010 <chw001025@gmail.com>
2025-07-03 23:27:12 +0900

    [Bronze V] Title: 상근이의 친구들, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int M, F;
+
+    while (1) {
+        scanf("%d %d", &M, &F);
+        if (M == 0 && F == 0) break;
+        printf("%d\n", M + F);
+    }
+
+    return 0;
+}
