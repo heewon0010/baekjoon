505360d7e6e2aa7b025cdf07c868a132013582d6
heewon0010 <chw001025@gmail.com>
2025-07-04 23:37:30 +0900

    [Bronze V] Title: 개수 세기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int N, v, count = 0;
+    scanf("%d", &N);
+
+    int arr[100];
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d", &v);
+
+    for (int i = 0; i < N; i++) {
+        if (arr[i] == v) count++;
+    }
+
+    printf("%d\n", count);
+
+    return 0;
+}
